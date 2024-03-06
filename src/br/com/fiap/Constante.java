a5aa2c0241ce21acfb6ace7078071e03ec266c1a
cotete <feliperibeirots@gmail.com>
2024-03-06 12:03:01 -0300

    Adicionado a classe constante, e estudo sobre constantes e suas convencoes.

---
+++ src/br/com/fiap/Constante.java
@@ -0,0 +1,10 @@
+package br.com.fiap;
+
+public class Constante {
+    public static void main(String[] args) {
+
+        final double PI = 3.1416;
+        final float GRAVIDADE_TERRESTRE = 9.8f;
+
+    }
+}
