5a2ad2c2a543b2b52e50842f872eea7864d44bef
cotete <feliperibeirots@gmail.com>
2024-03-06 11:45:44 -0300

    Estudo de tipos de variaveis, e print com concatenacao de variaveis

---
+++ src/br/com/fiap/Primitivo.java
@@ -0,0 +1,24 @@
+package br.com.fiap;
+
+public class Primitivo {
+    public static void main(String[] args){
+        //declaracao de variavel
+        char sexo;
+        //inicializacao = atribuir um valor
+        sexo = 'F';
+        //declarar e inicializar a var
+        byte idade = 23;
+        short codigo = 15650;
+        int alunos = 34;
+        int turmas = 11;
+        long brasileiros = 2121432532;
+        float media = 8.7f;
+        double cotacaoDoDolar = 4.95;
+        boolean alternativa = false; // true
+
+        System.out.println("Sexo: " + sexo + " \nidade: " + idade);
+
+
+    }
+
+}
