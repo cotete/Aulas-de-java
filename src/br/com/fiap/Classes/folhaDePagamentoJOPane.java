ba165aab675640c30bee572b8e6ac93a6bd4077f
cotete <feliperibeirots@gmail.com>
2024-03-20 11:18:29 -0300

    feito folhaDePagamentoJOPane

---
+++ src/br/com/fiap/Classes/folhaDePagamentoJOPane.java
@@ -0,0 +1,27 @@
+package br.com.fiap.Classes;
+
+import javax.swing.*;
+
+public class folhaDePagamentoJOPane {
+    public static void main(String[] args) {
+
+
+        FolhaDePagamento fdp;
+
+        try {
+            fdp = new FolhaDePagamento();
+            fdp.salarioBruto = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do seu salario bruto: "));
+            fdp.descontoINSS = Integer.parseInt(JOptionPane.showInputDialog("Digite o desconto do INSS: "));
+            fdp.nDeDependentes = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero de dependentes "));
+            fdp.valorPlanoDeSaude = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do plano de saude: "));
+
+            double salarioLiquido = fdp.calcularSalarioLiquido();
+
+            JOptionPane.showMessageDialog(null, "O seu salario liquido e de: " + salarioLiquido);
+
+        } catch (Exception e) {
+            System.out.println("Voce errou algo ai veionho melhore");
+        }
+
+    }
+}
