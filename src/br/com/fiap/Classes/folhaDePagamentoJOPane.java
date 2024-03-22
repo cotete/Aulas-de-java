8975e2419279314ae0acc846c80375bdc2ebe487
cotete <feliperibeirots@gmail.com>
2024-03-21 21:22:59 -0300

    resolvendo errinho no folhaDePagamentoJOPane

--- src/br/com/fiap/Classes/folhaDePagamentoJOPane.java
+++ src/br/com/fiap/Classes/folhaDePagamentoJOPane.java
@@ -1,28 +1,25 @@
 package br.com.fiap.Classes;
 
-import javax.swing.*;
+import javax.swing.JOptionPane;
 
 public class folhaDePagamentoJOPane {
     public static void main(String[] args) {
 
 
         FolhaDePagamento fdp = new FolhaDePagamento();
-        double salarioBruto, descontoINSS, valorPlanoDeSaude;
-        int numeroDeDependentes;
+        double x, z;
+        int w, y;
 
         try {
-            salarioBruto = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do seu salario bruto: "));
-            descontoINSS = Integer.parseInt(JOptionPane.showInputDialog("Digite o desconto do INSS: "));
-            numeroDeDependentes = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero de dependentes "));
-            valorPlanoDeSaude = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do plano de saude: "));
-            fdp.valorPlanoDeSaude = valorPlanoDeSaude;
-            fdp.nDeDependentes = numeroDeDependentes;
-            fdp.salarioBruto = salarioBruto;
-            fdp.descontoINSS = descontoINSS;
+            x = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do seu salario bruto: "));
+            w = Integer.parseInt(JOptionPane.showInputDialog("Digite o desconto do INSS: "));
+            y = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero de dependentes "));
+            z = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do plano de saude: "));
+            fdp.valorPlanoDeSaude = z;
+            fdp.nDeDependentes = y;
+            fdp.salarioBruto = x;
+            fdp.descontoINSS = w;
 
-            double salarioLiquido = fdp.calcularSalarioLiquido();
-
-            JOptionPane.showMessageDialog(null, "O seu salario liquido e de: " + salarioLiquido);
 
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Voce errou algo ai veionho melhore");
